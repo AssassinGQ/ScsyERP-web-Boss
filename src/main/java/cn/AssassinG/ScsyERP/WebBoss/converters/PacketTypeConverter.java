074d2275358146970278d45a0f0451f50418d0bb
AssassinG <hangq_zju@hotmail.com>
2019-03-19 20:11:21 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/WebBoss/converters/PacketTypeConverter.java
+++ src/main/java/cn/AssassinG/ScsyERP/WebBoss/converters/PacketTypeConverter.java
@@ -3,9 +3,10 @@ package cn.AssassinG.ScsyERP.WebBoss.converters;
 import cn.AssassinG.ScsyERP.BasicInfo.facade.enums.PacketType;
 import org.springframework.core.convert.converter.Converter;
 
-public class PacketTypeConverter implements Converter<Integer, PacketType> {
+public class PacketTypeConverter implements Converter<String, PacketType> {
     @Override
-    public PacketType convert(Integer value) {
-        return PacketType.getEnum(value);
+    public PacketType convert(String value) {
+        System.out.println("in PacketTypeConverter");
+        return PacketType.getEnum(Integer.parseInt(value));
     }
 }
