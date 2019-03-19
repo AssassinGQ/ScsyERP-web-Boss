81c1ed0306f921551369c7f4386e8b4be5ce251b
AssassinG <hangq_zju@hotmail.com>
2019-03-19 19:49:27 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/WebBoss/converters/ProductStatusConverter.java
+++ src/main/java/cn/AssassinG/ScsyERP/WebBoss/converters/ProductStatusConverter.java
@@ -3,9 +3,9 @@ package cn.AssassinG.ScsyERP.WebBoss.converters;
 import cn.AssassinG.ScsyERP.BasicInfo.facade.enums.ProductStatus;
 import org.springframework.core.convert.converter.Converter;
 
-public class ProductStatusConverter implements Converter<String, ProductStatus> {
+public class ProductStatusConverter implements Converter<Integer, ProductStatus> {
     @Override
-    public ProductStatus convert(String s) {
+    public ProductStatus convert(Integer s) {
         return ProductStatus.getEnum(s);
     }
 }
